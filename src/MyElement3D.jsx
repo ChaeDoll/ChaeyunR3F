06d1841b65266cdd767cb6af7d3cd252912d1aaa
ChaeDoll <dlacodbs0627@gmail.com>
2024-04-11 03:21:56 +0900

    240411 r3f practice start

---
+++ src/MyElement3D.jsx
@@ -0,0 +1,33 @@
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react"
+import * as THREE from "three";
+
+export default function MyElement3D() {
+    const meshRef = useRef();
+    useFrame((state, delta)=>{
+        meshRef.current.rotation.x += delta;
+    })
+    return (
+        <>
+            <directionalLight position={[1, 1, 1]}/>
+            <axesHelper scale={10}/>
+            <OrbitControls/>
+            <mesh ref={meshRef} 
+            rotation={[THREE.MathUtils.degToRad(15),THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(20)]} 
+            scale={[1, 1, 2]} position={[0, 0, 0]}>
+                <boxGeometry/>
+                <meshStandardMaterial color={'#e67e22'} opacity={0.5} transparent={true}/>
+                <axesHelper />
+                <mesh
+                rotate
+                scale={[0.1, 0.1, 0.1]}
+                position={[2, 2, 0]}>
+                    <sphereGeometry/>
+                    <meshStandardMaterial color={"red"}/>
+                    <axesHelper scale={3}/>
+                </mesh>
+            </mesh>
+        </>
+    )
+}
