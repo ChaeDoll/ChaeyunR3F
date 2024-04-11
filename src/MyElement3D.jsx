2dbc697d55911c6f33c2b5148137b877f2e5a120
ChaeDoll <dlacodbs0627@gmail.com>
2024-04-11 22:07:17 +0900

    240411 practice

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,38 +1,32 @@
-import { Box, OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls, useTexture } from "@react-three/drei";
 import { useControls } from "leva";
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState, useEffect } from "react"
 import * as THREE from "three";
 
 export default function MyElement3D({rotation}) {
-    const meshRef = useRef();
-    const wireMeshRef = useRef();
-    const { xSize, ySize, zSize, xSegments, ySegments, zSegments } = useControls({
-        xSize: { value:1, min:0.1, max:5, step:0.01},
-        ySize: { value:1, min:0.1, max:5, step:0.01},
-        zSize: { value:1, min:0.1, max:5, step:0.01},
-        xSegments: { value:1, min:1, max:10, step:1},
-        ySegments: { value:1, min:1, max:10, step:1},
-        zSegments: { value:1, min:1, max:10, step:1}
-    })
+    const boxRef = useRef();
+    const torusRef = useRef();
     useEffect(()=>{
-        wireMeshRef.current.geometry = meshRef.current.geometry;
-    },[xSize, ySize, zSize, xSegments, ySegments, zSegments])
+        torusRef.current.material = boxRef.current.material;
+    },[])
+
+    const texture = useTexture("./images/threeTone.jpg");
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
     return (
         <>
             <OrbitControls/>
-            <directionalLight position={[2, 1, 3]}/>
-            <axesHelper scale={10}/>
-            <mesh ref={meshRef} 
-            rotation={[THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0)]} 
-            scale={[1, 1, 1]} position={[0, 0, 0]}>
-                <boxGeometry args={[xSize, ySize, zSize, xSegments, ySegments, zSegments]}/>
-                <meshStandardMaterial color={'#FFFFFF'}/>
-                <axesHelper />
+            <ambientLight intensity={0.2}/>
+            <directionalLight position={[0,1,0]}/>
+            <directionalLight position={[1,2,8]} intensity={0.7}/>
+            {/* <axesHelper scale={10}/> */}
+            <mesh ref={boxRef} position={[-1, 0, 0]}>
+                <torusKnotGeometry/>
+                <meshToonMaterial gradientMap={texture} color={"cyan"}/>
             </mesh>
-            <mesh ref={wireMeshRef}
-            position={[0, 0, 0]}>
-                <meshStandardMaterial emissive="yellow" wireframe={true}/>
+            <mesh ref={torusRef} position={[3, 0, 0]}>
+                <torusGeometry/>
             </mesh>
         </>
     )
