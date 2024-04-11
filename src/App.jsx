cd3f2a20a1648c5e0a21ae5b668a2930630a97e0
ChaeDoll <dlacodbs0627@gmail.com>
2024-04-11 17:25:04 +0900

    240411 r3f

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Canvas } from '@react-three/fiber'
 import './App.css'
 import MyElement3D from './MyElement3D'
+import { useState } from 'react';
 
 function App() {
-
+  const [rotation, setRotation] = useState(0);
+  const handleTurnLeft = () => {
+    setRotation(prev=>prev+0.001);
+  }
+  const handleTurnRight = () => {
+    setRotation(prev=>prev-0.001);
+  }
   return (
     <>
       <Canvas>
-        <MyElement3D/>
+        <MyElement3D rotation={rotation}/>
       </Canvas>
+      <button onClick={handleTurnLeft}>왼쪽으로 이동</button>
+      <button onClick={handleTurnRight}>오른쪽으로 이동</button>
     </>
   )
 }
--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -3,26 +3,26 @@ import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react"
 import * as THREE from "three";
 
-export default function MyElement3D() {
+export default function MyElement3D({rotation}) {
     const meshRef = useRef();
     useFrame((state, delta)=>{
-        meshRef.current.rotation.x += delta;
+        meshRef.current.rotation.y += rotation;
     })
     return (
         <>
-            <directionalLight position={[1, 1, 1]}/>
+            <directionalLight position={[2, 2, 2]}/>
             <axesHelper scale={10}/>
             <OrbitControls/>
             <mesh ref={meshRef} 
-            rotation={[THREE.MathUtils.degToRad(15),THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(20)]} 
-            scale={[1, 1, 4]} position={[0, 0, 0]}>
+            rotation={[THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0)]} 
+            scale={[1, 1, 1]} position={[0, 0, 0]}>
                 <boxGeometry/>
                 <meshStandardMaterial color={'#e67e22'} opacity={0.5} transparent={true}/>
                 <axesHelper />
                 <mesh
                 rotate
                 scale={[0.2, 0.3, 0.3]}
-                position={[2, 3, 0]}>
+                position={[2, 0, 0]}>
                     <sphereGeometry/>
                     <meshStandardMaterial color={"red"}/>
                     <axesHelper scale={3}/>
