6d76059453ad74f7e73f6945d14f25a229120e3c
ChaeDoll <dlacodbs0627@gmail.com>
2024-04-15 20:05:20 +0900

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { ARButton, Controllers, XR, XRButton } from '@react-three/xr';
+import { ARButton, Controllers, XR, XRButton, useHitTest } from '@react-three/xr';
 import './App.css'
 import { Canvas } from '@react-three/fiber';
 import ARTestContainer from './ARTestContainer';
+import { useRef } from 'react';
+import { Box } from '@react-three/drei';
+
+export function HitTestExample(props) {
+  const boxRef = useRef(null);
+  useHitTest((hitMatrix) => {
+    if (boxRef.current) {
+      hitMatrix.decompose(boxRef.current.position, boxRef.current.quaternion, boxRef.current.scale)
+    }
+  })
+  return <Box ref={boxRef} {...props} args={[0.1, 0.1, 0.1]} />
+}
 
 function App() {
   return (
     <>
-      <ARButton>
+      <ARButton sessionInit={{
+      optionalFeatures:["hit-test"]
+      }}>
         <Canvas>
-          <XR referenceSpace='local'>
+          <XR>
             <ambientLight />
-            <ARTestContainer />
+            <pointLight position={[5, 5, 5]} />
+            <Controllers />
+            <HitTestExample position={[0, 0.8, -1]} />
+            {/* <ARTestContainer /> */}
           </XR>
         </Canvas>
-    </ARButton>
+      </ARButton>
     </>
   )
 }
-
 export default App;
